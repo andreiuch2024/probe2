26436e19bc5e4afa330b60b5bd3f5cddbafb753e
Andrei <a.muravjev1515@yndex.ru>
2024-01-04 17:46:24 +0300

    cycle_for_01

---
+++ cycle_for.py
@@ -0,0 +1,8 @@
+car_list = ['BMW', 'MB', 'LADA', 'KIA', 'HONDA']
+for stamp in car_list:
+    print('Я езжу на автомобиле марки', stamp)
+cars_count = 0
+for stamp in car_list:
+    cars_count += 10
+    pass
+print('Всего автомобилей', cars_count)
