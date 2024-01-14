94c276cfa401250ccaf79896d031eaa2bf95d3ac
andreiuch2024 <154827116+andreiuch2024@users.noreply.github.com>
2024-01-14 15:22:17 +0300

    Update cycle_for.py

--- cycle_for.py
+++ cycle_for.py
@@ -1,8 +1,12 @@
-car_list = ['BMW', 'MB', 'LADA', 'KIA', 'HONDA']
+car_list = ['BMW', 'MB', 'LADA', 'KIA', 'HONDA', 'TOYOTA']
 for stamp in car_list:
     print('Я езжу на автомобиле марки', stamp)
+    if stamp == 'TOYOTA':
+         print('Это верно! :)')
+         break
+    print('Это не верно...')
 cars_count = 0
 for stamp in car_list:
     cars_count += 10
     pass
-print('Всего автомобилей', cars_count)
+print('Всего автомобилей', cars_count, 'шт')
